890862de869ea956a674db0a06d182e8a7d93b02
Brent Ahrens <cidida@gmail.com>
2019-03-17 19:46:47 -0700

    another push with build

---
+++ build/precache-manifest.db7ad7ca3f3b187ccdd68623dbab7fd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7a1fecbf15bc3c3e4d1",
+    "url": "/static/js/main.fd51790b.chunk.js"
+  },
+  {
+    "revision": "133401ee7f1513520de1",
+    "url": "/static/js/2.9ae24802.chunk.js"
+  },
+  {
+    "revision": "a7a1fecbf15bc3c3e4d1",
+    "url": "/static/css/main.522eb72c.chunk.css"
+  },
+  {
+    "revision": "27aeef12c0fb3375649b6bfbba94d559",
+    "url": "/index.html"
+  }
+];
--- src/Education/Education.js
+++ src/Education/Education.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Education.css'
 
-console.log('hello')
 const Education = () => {
     return(
         <div className="education-container">
